e41326d171fe8d2fa24d917ba7fd82e9f04a562f
FelipePonceB <felipeponcebenites@gmail.com>
2024-07-17 02:27:58 +0200

    adding animation

---
+++ wp-content/plugins/lottiefiles/src/common.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\mkb\wp-content\plugins\lottiefiles\src\common.php:2
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\mkb\wp-content\plugins\lottiefiles\src\common.php</b> on line <b>2</b><br>
