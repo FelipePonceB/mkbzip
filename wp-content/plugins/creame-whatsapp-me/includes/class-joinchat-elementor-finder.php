a51aa9c09d0c58db486949e06cc25d9c4894fde1
FelipePonceB <felipeponcebenites@gmail.com>
2024-11-18 23:21:09 +0100

    Fixed hero and spacing

---
+++ wp-content/plugins/creame-whatsapp-me/includes/class-joinchat-elementor-finder.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;Elementor\Core\Common\Modules\Finder\Base_Category&quot; not found in C:\xampp\htdocs\mkb\wp-content\plugins\creame-whatsapp-me\includes\class-joinchat-elementor-finder.php:16
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\mkb\wp-content\plugins\creame-whatsapp-me\includes\class-joinchat-elementor-finder.php</b> on line <b>16</b><br>
